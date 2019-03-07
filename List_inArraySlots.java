26999c82b24d38aee28de62f8cbfde2a7f427d3f
afmkhan29 <akhan00@stuy.edu>
2019-03-06 22:13:09 -0500

    hw submission!

---
+++ List_inArraySlots.java
@@ -0,0 +1,74 @@
+/**
+  Implement a list of integer elements, including
+  both data and operations.
+ */
+
+public class List_inArraySlots {
+
+    // declare fields here
+    int filledCount = 0;
+    int currentMax = 10;
+    int[] listed = new int[currentMax];
+    
+
+    /**
+      Construct an empty list with a small initial capacity.
+     */
+    public List_inArraySlots() {
+    }
+
+
+    /** 
+      @return the number of elements in this list
+     */
+    public int size() {
+        return filledCount;
+    }
+
+
+     /** 
+       @return a string representation of this list,
+       in [a,b,c,] format
+      */ 
+    public String toString() {
+        String total = "";
+        for (int i: listed) {
+            total += i + " ";
+        }
+        return total;
+    }
+
+    
+    /** 
+      Appends @value to the end of this list.
+      
+      @return true, in keeping with conventions yet to be discussed
+     */
+     public boolean add( int value) {
+        if (filledCount == currentMax) {
+            System.out.println("somethings up...");
+            expand();
+            
+        }
+        listed[filledCount++] = value;
+
+
+        return true;
+       
+     }
+
+
+    /** 
+      Double the capacity of the List_inArraySlots, 
+      preserving existing data
+     */
+     private void expand() {
+        int[] dataHolder = new int[currentMax * 2];
+        currentMax *= 2;
+        for (int i = 0; i < listed.length; i++) {
+            dataHolder[i] = listed[i];
+            System.out.println(i);
+        }
+        listed = dataHolder;
+     }
+}
